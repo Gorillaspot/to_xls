77b114ac1e0c3aefba0bd6a4365f5af9cd150864
James Kassemi <jkassemi@gmail.com>
2012-01-19 23:45:28 -0700

    Changed method name back

--- lib/to_xls/array_patch.rb
+++ lib/to_xls/array_patch.rb
@@ -4,6 +4,6 @@ require 'to_xls/array_writer.rb'
 class Array
   # Options for to_xls: columns, name, header, sheet
   def to_xls(options = {})
-    ToXls::ArrayWriter.new(self, options).to_s
+    ToXls::ArrayWriter.new(self, options).write_string
   end
 end
